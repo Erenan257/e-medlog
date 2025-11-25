e54ca47da10b5905fe566bb56a2c69b931ec0b98
Renan Souza rasp <erenan257@gmail.com>
2025-11-25 01:36:20 -0300

    fix(prod): Correções de ProxyFix e rotas feitas no Raspberry

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,17 @@
-# backend/run.py
-
 from app import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix   
 
 app = create_app()
 
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_for=1,
+    x_proto=1,
+    x_host=1,
+    x_port=1
+)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
+
